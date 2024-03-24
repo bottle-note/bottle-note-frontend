e1e8bfb02d4fa509205a1016722685645a11be68
hyejj19@naver.com <hyejj19@naver.com>
2024-03-24 17:21:41 +0900

    [Feat] add Kakao Login

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: `${process.env.KAKAO_CLIENT_ID}`,
+      clientSecret: `${process.env.KAKAO_CLIENT_SECRET}`,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+
+    async session({ session, token }) {
+      return { ...session, user: token };
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
