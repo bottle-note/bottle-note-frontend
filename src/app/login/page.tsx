d4d25813ef011c350d14d836297daaa80797402e
hyejj19@naver.com <hyejj19@naver.com>
2024-03-25 10:15:59 +0900

    [Feat] add Logout function

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,10 @@
 'use client';
 
 import Image from 'next/image';
-import { signIn } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Login() {
-  const handleKakao = async () => {
-    await signIn('kakao', {
-      redirect: true,
-      callbackUrl: '/',
-    });
-  };
+  const { data: session } = useSession();
 
   return (
     <main className="w-full h-[100vh] flex flex-col justify-center items-center">
@@ -20,10 +15,17 @@ export default function Login() {
         alt="bottle-note-logo"
       />
       <section className="flex flex-col mt-5 space-y-3">
-        <button onClick={handleKakao}>카카오톡 로그인</button>
+        <button onClick={() => signIn('kakao')}>카카오톡 로그인</button>
         <button>네이버 로그인</button>
         <button>구글 로그인</button>
         <button>애플 로그인</button>
+      </section>
+      <section className="flex flex-col mt-5 space-y-3">
+        {session && session.user && (
+          <button onClick={() => signOut()}>
+            {session.user.name}님 로그아웃
+          </button>
+        )}
       </section>
     </main>
   );
