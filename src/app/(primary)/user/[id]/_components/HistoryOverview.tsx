f4e1c8e973bb7c2e99990a5b4c1ec5c8a2534d39
hyejj19@naver.com <hyejj19@naver.com>
2024-04-21 16:43:02 +0900

    [Modify] add HistoryOverview component props

--- src/app/(primary)/user/[id]/_components/HistoryOverview.tsx
+++ src/app/(primary)/user/[id]/_components/HistoryOverview.tsx
@@ -1,16 +1,22 @@
-const MOCK_HISTORY_OVERVIEW = [
-  { name: '별점', value: 53 },
-  { name: '리뷰', value: 12 },
-  { name: '찜하기', value: 6 },
-];
+// TODO: 실제 api dto 에 맞출것
+interface Props {
+  rates: number;
+  reviews: number;
+  likes: number;
+}
 
-// FIXME: name 이 api 응답으로 오는 값에 따라 동적으로 변하도록 수정 필요
-const HistoryOverview = () => {
+const HistoryOverview = ({ rates, reviews, likes }: Props) => {
+  const MOCK_HISTORY_OVERVIEW = [
+    { name: '별점', value: rates },
+    { name: '리뷰', value: reviews },
+    { name: '찜하기', value: likes },
+  ];
+
   return (
     <article className="flex justify-center pt-2.75 divide-x divide-subCoral divide-opacity-30 text-fontBurgundy">
       {MOCK_HISTORY_OVERVIEW.map((item) => (
         <p className="flex flex-col items-center px-8.5" key={item.name}>
-          <span className="text-[2.125rem] font-bold text-[#DF762A]">
+          <span className="text-[2.125rem] font-bold text-subCoral">
             {item.value}
           </span>
           <span className="text-sm whitespace-nowrap">{item.name}</span>
