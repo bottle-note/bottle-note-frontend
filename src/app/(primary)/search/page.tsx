c649e8ee77138ad871bc830de96e603a637f4586
hyejj19@naver.com <hyejj19@naver.com>
2024-05-16 21:49:05 +0900

    [Style] add RecentSearch
    * SearchHeader 컴포넌트 없앰
    * Search page 클라이언트 컴포넌트로 변경 (유저 인터랙션에 따른 렌더링 변경)

--- src/app/(primary)/search/page.tsx
+++ src/app/(primary)/search/page.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+'use client';
+
+import React, { useState } from 'react';
 import { MOCK_LIST_ITEM } from 'mock/alcohol';
 import CategorySelector from '@/components/CategorySelector';
 import CategoryTitle from '@/components/CategoryTitle';
+import SearchBar from '@/components/SearchBar';
 import SearchList from './_components/SearchList';
-import SearchHeader from '../_components/SearchHeader';
-
-// TODO: it goes like...
-// 1. 검색어를 입력 후 확인을 누른다.
-// 2. 검색어가 입력 되자마자 최근 검색어가 나타나야 한다.
-// 3. 확인을 누르면 서치 파라미터 변경되고 검색 결과를 띄워준다.
-// 4. 구조가 복잡한데 도대체 서버 컴포넌트는 왜 쓰는거지...? ㅋㅋㅋㅋㅋㅋㅋㅋㅋ ㅇ<-<
+import RecentSearch from '../_components/RecentSearch';
 
 export default function Search() {
-  // TODO: api call here
+  const [isOnFocus, setIsOnFocus] = useState(false); // FIXME: 이걸 state로 관리하는게 맞을지 고민
+
+  const handleSearch = (value: string) => {
+    console.log(value);
+    // TODO: api call here
+  };
 
   return (
     // FIXME: 검색 화면에 스크롤이 생기면 안될 것 같은데...
     <main className="mb-24 w-full h-full">
-      <SearchHeader />
+      <div
+        className="px-5 pt-[76px] pb-6 bg-subCoral"
+        onClick={() => setIsOnFocus(true)}
+      >
+        <SearchBar handleSearch={handleSearch} />
+      </div>
 
       <section className="p-5 space-y-7">
-        <CategorySelector currentValue="전체" />
-        <section>
-          <CategoryTitle subTitle="위클리 HOT 5" />
-          <SearchList data={MOCK_LIST_ITEM} />
-        </section>
+        {isOnFocus ? (
+          <RecentSearch />
+        ) : (
+          <>
+            <CategorySelector currentValue="전체" />
+            <section>
+              <CategoryTitle subTitle="위클리 HOT 5" />
+              <SearchList data={MOCK_LIST_ITEM} />
+            </section>
+          </>
+        )}
       </section>
     </main>
   );
