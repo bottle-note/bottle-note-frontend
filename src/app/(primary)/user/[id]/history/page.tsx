9eb8dda073f5782b3f57df3e01257855d36a47ef
hyejj19@naver.com <hyejj19@naver.com>
2024-06-07 20:41:45 +0900

    [Feat] 히스토리 페이지 Tab 컴포넌트 추가

--- src/app/(primary)/user/[id]/history/page.tsx
+++ src/app/(primary)/user/[id]/history/page.tsx
@@ -1,5 +1,53 @@
-import React from 'react';
+'use client';
+
+import { SubHeader } from '@/app/(primary)/_components/SubHeader';
+import Tab from '@/components/Tab';
+import { HISTORY_TYPES } from '@/constants/user';
+import { useTab } from '@/hooks/useTab';
+import Image from 'next/image';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 export default function UserHistory() {
-  return <div>UserHistory page</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const historyType = searchParams.get('type');
+
+  const { currentTab, handleTab, tabList } = useTab({ tabList: HISTORY_TYPES });
+
+  useEffect(() => {
+    const selected = tabList.find((item) => item.id === historyType);
+
+    handleTab(selected?.id ?? 'all');
+  }, [historyType]);
+
+  useEffect(() => {
+    router.push(`?type=${currentTab.id}`);
+  }, [currentTab]);
+
+  return (
+    <main>
+      <SubHeader bgColor="bg-bgGray">
+        <SubHeader.Left
+          onClick={() => {
+            router.back();
+          }}
+        >
+          <Image
+            src="/icon/arrow-left-subcoral.svg"
+            alt="arrowIcon"
+            width={23}
+            height={23}
+          />
+        </SubHeader.Left>
+        <SubHeader.Center textColor="text-subCoral">
+          마이페이지
+        </SubHeader.Center>
+      </SubHeader>
+
+      <section className="pt-10 px-5">
+        <Tab currentTab={currentTab} handleTab={handleTab} />
+      </section>
+    </main>
+  );
 }
