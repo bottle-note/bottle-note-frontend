17180c271d8efc1f218f53b83e3cb1f656aafc26
hyejj19@naver.com <hyejj19@naver.com>
2024-06-09 12:21:13 +0900

    [Chore] 리프레시 토큰 갱신 관련 수도코드 추가

--- src/utils/fetchWithAuth.ts
+++ src/utils/fetchWithAuth.ts
@@ -1,5 +1,6 @@
 import { getSession } from 'next-auth/react';
 import { accessTokenService } from './TokenService';
+import { ApiResponse } from '@/types/common';
 
 export async function fetchWithAuth(url: string, options?: RequestInit) {
   const defaultOptions = {
@@ -11,23 +12,35 @@ export async function fetchWithAuth(url: string, options?: RequestInit) {
     },
   };
 
-  // TODO: 캐치블록 내부로 이동
-  const session = await getSession();
-  console.log(session);
-
   const requestUrl = `${url}`;
 
   try {
     const response = await fetch(requestUrl, defaultOptions);
 
     if (!response.ok) {
+      const res: ApiResponse<any> = await response.json();
+
+      // case 1: 에러 코드가 403인 경우 -> 기간 만료이므로 리프레시 토큰으로 갱신
+      if (res.code === 403) {
+        const session = await getSession();
+        return console.log(
+          session?.user.token.refreshToken,
+          '리프레시 토큰은 여기에 있어용',
+        );
+      }
+
+      // case 2: 에러 코드가 401인 경우 -> 인증된 유저가 아니므로 로그인 페이지로 이동?
+      if (res.code === 401) {
+        return console.log(res);
+      }
+
+      // case 3: 그 이외의 에러는 throw
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     return response.json();
   } catch (error) {
-    // TODO: 에러가 발생했을 때 리프레시 토큰으로 갱신 요청
-
+    // TODO: 적절한 에러처리 필요!
     console.error('Fetch error:', error);
   }
 }
