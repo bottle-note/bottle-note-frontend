1f0dd79745a2bcd45b868e613ff1a8c367e0bbb4
hyejj19@naver.com <hyejj19@naver.com>
2024-07-11 22:25:22 +0900

    [Add] 리스트 아이템 클릭시 detail 페이지로 이동 링크 추가

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,13 +1,11 @@
-'use client';
-
 import Star from '@/components/Star';
 import { truncStr } from '@/utils/truncStr';
 import LikeBtn from '@/app/(primary)/user/[id]/_components/LikeBtn';
-import Review from '@/app/(primary)/user/[id]/_components/ReviewBtn';
 import { addNewLine } from '@/utils/addNewLine';
 import ItemImage from './_components/ItemImage';
 import ItemInfo from './_components/ItemInfo';
 import { AlcoholAPI } from '@/types/Alcohol';
+import Link from 'next/link';
 
 interface Props {
   data: AlcoholAPI;
@@ -16,38 +14,42 @@ interface Props {
 const ListItem = ({ data }: Props) => {
   const {
     korCategoryName,
+    engCategoryName,
     korName,
     engName,
     imageUrl,
     rating,
     isPicked,
     ratingCount,
+    alcoholId,
   } = data;
 
   return (
-    <article className="flex items-center space-x-2 text-mainBlack border-mainBlack border-b h-[90px]">
-      <ItemImage src={imageUrl} alt="위스키 이미지" />
-      <section className="flex-1 space-y-1">
-        <ItemInfo
-          korName={addNewLine(korName)}
-          engName={truncStr(engName, 16)}
-          korCategory={korCategoryName}
-        />
-        <article className="flex justify-between">
-          <div className="flex flex-col">
-            <Star rating={rating} />
-            {ratingCount && (
-              <span className="text-[0.625rem] justify-self-end row-start-2 text-right">
-                {`${ratingCount}`}
-              </span>
-            )}
-          </div>
-          <div className="space-x-1.5 flex items-center">
-            <LikeBtn isLiked={isPicked} />
-          </div>
-        </article>
-      </section>
-    </article>
+    <Link href={`/search/${engCategoryName}/${alcoholId}`}>
+      <article className="flex items-center space-x-2 text-mainBlack border-mainBlack border-b h-[90px]">
+        <ItemImage src={imageUrl} alt="위스키 이미지" />
+        <section className="flex-1 space-y-1">
+          <ItemInfo
+            korName={addNewLine(korName)}
+            engName={truncStr(engName, 16)}
+            korCategory={korCategoryName}
+          />
+          <article className="flex justify-between">
+            <div className="flex flex-col">
+              <Star rating={rating} />
+              {ratingCount && (
+                <span className="text-[0.625rem] justify-self-end row-start-2 text-right">
+                  {`${ratingCount}`}
+                </span>
+              )}
+            </div>
+            <div className="space-x-1.5 flex items-center">
+              <LikeBtn isLiked={isPicked} />
+            </div>
+          </article>
+        </section>
+      </article>
+    </Link>
   );
 };
 
