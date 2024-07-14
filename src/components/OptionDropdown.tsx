7136e6dad2d70e270d17a8ca8a5c984a2017e034
hyejj19@naver.com <hyejj19@naver.com>
2024-07-15 01:35:32 +0900

    [Modify] 프로필 수정 페이지 변경된 OptionDropdown 수정사항 적용

--- src/components/OptionDropdown.tsx
+++ src/components/OptionDropdown.tsx
@@ -4,22 +4,21 @@ interface Props {
   handleClose: () => void;
   options: { type: string; name: string }[];
   handleOptionSelect: ({ type, name }: { type: string; name: string }) => void;
+  title?: string;
 }
 
-// TODO: 옵션, 타입 props 로 받아오도록 수정
 export default function OptionDropdown({
   handleClose,
   options,
   handleOptionSelect,
+  title = '정렬',
 }: Props) {
-  const type = '정렬';
-
   return (
     <BackDrop isShow>
       <div className="w-full h-full flex flex-col justify-end items-center px-4 gap-3 pb-2">
         <section className="w-full bg-white rounded-xl divide-y max-h-[400px] overflow-y-scroll">
           <article className="py-4 text-center text-mainGray text-sm">
-            {type}
+            {title}
           </article>
           {options.map((option) => (
             <article
