58de2fe23cf4aeb49533c9eef74e42b454270657
sareum <rkdtkfma@gmail.com>
2024-08-11 00:12:28 +0900

    [Feat] Implement comment and reply functionality and connect to API

--- src/types/Reply.ts
+++ src/types/Reply.ts
@@ -7,3 +7,43 @@ export interface RootReply {
   subReplyCount: number;
   createAt: string;
 }
+
+export interface RootReplyListApi {
+  reviewReplies: RootReply[];
+  totalCount: number;
+}
+
+export interface SubReply {
+  userId: number;
+  imageUrl: string;
+  nickName: string;
+  rootReviewId: number;
+  parentReviewReplyId: number;
+  parentReviewReplyAuthor: string;
+  reviewReplyId: number;
+  reviewReplyContent: string;
+  createAt: string;
+}
+
+export interface SubReplyListApi {
+  reviewReplies: SubReply[];
+  totalCount: number;
+}
+
+export interface ReplyQueryParams {
+  content: string;
+  parentReplyId?: string | null;
+}
+
+export interface ReplyPostApi {
+  codeMessage: string;
+  message: string;
+  reviewId: number;
+  responseAt: string;
+}
+
+export interface FormValues {
+  content: string;
+  parentReplyId?: string | null;
+  replyToReplyUserName?: string | null;
+}
