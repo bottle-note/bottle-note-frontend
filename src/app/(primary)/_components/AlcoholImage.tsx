f1e0033140b667f5ade8e0b2dee2d3b020640003
sareum <rkdtkfma@gmail.com>
2024-11-20 23:05:32 +0900

    [Modify] Added props for width and height to enhance reusability.

--- src/app/(primary)/_components/AlcoholImage.tsx
+++ src/app/(primary)/_components/AlcoholImage.tsx
@@ -1,9 +1,25 @@
 import Image from 'next/image';
 
-const AlcoholImage = ({ imageUrl }: { imageUrl: string }) => (
+interface Props {
+  imageUrl: string;
+  outerHeightClass?: string;
+  outerWidthClass?: string;
+  innerHeightClass?: string;
+  innerWidthClass?: string;
+}
+
+const AlcoholImage = ({
+  imageUrl,
+  outerHeightClass = 'h-[162px]',
+  outerWidthClass = 'w-[100px]',
+  innerHeightClass = 'w-[80px] ',
+  innerWidthClass = 'h-[140px]',
+}: Props) => (
   <div className="rounded-lg bg-white p-2 flex items-center justify-center">
-    <article className="h-[162px] w-[100px] shrink-0 relative flex items-center justify-center">
-      <div className="relative w-[80px] h-[140px]">
+    <article
+      className={`${outerHeightClass} ${outerWidthClass} shrink-0 relative flex items-center justify-center`}
+    >
+      <div className={`${innerHeightClass} ${innerWidthClass} relative`}>
         <Image
           priority
           src={imageUrl}
