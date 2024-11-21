f9288226001e480c9999648e8be803e882994d3f
Hyejung Park <hyejj19@naver.com>
2024-11-21 15:19:29 +0900

    [#168] 로그인 페이지 UI 수정 (#181)
    * [Style] change logo color white
    
    * [Style] Modify Login page logo, btn style
    
    * [Style] Modify Social login button icon
    
    * [Style] Add SubHeader in Login Page
    
    * [Add] Alert when click email login
    
    - 준비중인 기능
    
    * [Style] Delete max-width
    
    * [Style] Apply loading component when redirect kakao login

--- src/app/(custom)/login/_components/SocialLoginBtn.tsx
+++ src/app/(custom)/login/_components/SocialLoginBtn.tsx
@@ -1,21 +1,49 @@
 import Image from 'next/image';
-import KakaoBtn from 'public/login/loginButton_kakao.svg';
-import AppleBtn from 'public/login/loginButton_apple.svg';
-import NaverBtn from 'public/login/loginButton_Naver.svg';
-import GoogleBtn from 'public/login/loginButton_google.svg';
+import KakaoLogo from 'public/icon/kakao-logo.svg';
+import AppleLogo from 'public/icon/apple-logo.svg';
 
+type LoginType = 'KAKAO' | 'APPLE' | 'GOOGLE' | 'NAVER';
 interface Props {
-  type: 'KAKAO' | 'APPLE' | 'GOOGLE' | 'NAVER';
+  type: LoginType;
   onClick: () => void;
 }
 
 function SocialLoginBtn({ type, onClick }: Props) {
+  const getStyle = (loginType: LoginType) => {
+    switch (loginType) {
+      case 'KAKAO':
+        return 'bg-[#FEE500] text-black text-opacity-85';
+      case 'APPLE':
+        return 'bg-black text-white';
+      default:
+    }
+  };
+
   return (
-    <button onClick={onClick}>
-      {type === 'KAKAO' && <Image src={KakaoBtn} alt="카카오로 로그인" />}
-      {type === 'APPLE' && <Image src={AppleBtn} alt="애플로 로그인" />}
-      {type === 'NAVER' && <Image src={NaverBtn} alt="네이버로 로그인" />}
-      {type === 'GOOGLE' && <Image src={GoogleBtn} alt="구글로 로그인" />}
+    <button
+      onClick={onClick}
+      className={`w-full rounded-md py-2.5 ${getStyle(type)}`}
+    >
+      {type === 'KAKAO' && (
+        <div className="flex justify-center relative">
+          <Image
+            src={KakaoLogo}
+            alt="kakao-logo"
+            className="absolute top-1/2 -translate-y-1/2 left-4 w-5"
+          />
+          <span>카카오 로그인</span>
+        </div>
+      )}
+      {type === 'APPLE' && (
+        <div className="flex justify-center relative">
+          <Image
+            src={AppleLogo}
+            alt="apple-logo"
+            className="absolute top-1/2 -translate-y-1/2 left-4 w-4"
+          />
+          <span>Apple로 로그인</span>
+        </div>
+      )}
     </button>
   );
 }
