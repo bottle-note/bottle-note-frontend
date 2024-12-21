b793155b0e6f27f7f9438e6dd4f684ed0b7e4750
sareum <rkdtkfma@gmail.com>
2024-12-22 01:02:17 +0900

    [Style]Change header of home padding top size.

--- src/app/(primary)/_components/Header.tsx
+++ src/app/(primary)/_components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   });
 
   return (
-    <div className="py-[1.3rem] px-5 bg-subCoral pt-12">
+    <div className="py-[1.3rem] px-5 bg-subCoral pt-14">
       <div
         className={`transition-opacity duration-500 ease-in-out flex items-center space-x-1 ${
           scrollPosition > 0 ? 'opacity-0 delay-150' : 'opacity-100'
