e5ca58f64200e2f4388b35b229bd15be75faa469
hyejj19 <hyejj19@naver.com>
2024-12-31 11:43:02 +0900

    feat: navigate to user page  when click follow user

--- src/app/(primary)/user/[id]/_components/FollowerListItem.tsx
+++ src/app/(primary)/user/[id]/_components/FollowerListItem.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { RelationInfo } from '@/types/User';
 import { FollowButton } from './FollowButton';
 import DefaultProfile from 'public/profile-default.svg';
@@ -13,29 +14,35 @@ export const FollowerListItem = ({ userInfo }: Props) => {
   return (
     <article
       key={userInfo.followUserId}
-      className="border-b border-brightGray py-3 grid grid-cols-[0.45fr_2fr_1fr]"
+      className="border-b border-brightGray py-3 flex justify-around"
     >
-      <Image
-        src={userInfo.userProfileImage ?? DefaultProfile}
-        width={36}
-        height={36}
-        alt="유저 프로필"
-        className="rounded-full"
-      />
-      <div className="flex flex-col gap-1">
-        <p className="text-12 font-bold">{userInfo.nickName}</p>
-        <p className="text-10 text-brightGray font-semibold flex gap-2">
-          <span className="flex gap-1">
-            <Image src={CommentGray} alt="리뷰" width={12} height={12} />
-            {`리뷰 ${userInfo.reviewCount}개`}
-          </span>
-          <span>|</span>
-          <span className="flex gap-1">
-            <Image src={StarGray} alt="평가" width={13} height={13} />
-            {`평가 ${userInfo.ratingCount}개`}
-          </span>
-        </p>
-      </div>
+      <Link
+        className="grid grid-cols-[0.45fr_1fr]"
+        href={`/user/${userInfo.userId}`}
+      >
+        <Image
+          src={userInfo.userProfileImage ?? DefaultProfile}
+          width={36}
+          height={36}
+          alt="유저 프로필"
+          className="rounded-full"
+        />
+        <div className="flex flex-col gap-1">
+          <p className="text-12 font-bold">{userInfo.nickName}</p>
+          <p className="text-10 text-brightGray font-semibold flex gap-2">
+            <span className="flex gap-1">
+              <Image src={CommentGray} alt="리뷰" width={12} height={12} />
+              {`리뷰 ${userInfo.reviewCount}개`}
+            </span>
+            <span>|</span>
+            <span className="flex gap-1">
+              <Image src={StarGray} alt="평가" width={13} height={13} />
+              {`평가 ${userInfo.ratingCount}개`}
+            </span>
+          </p>
+        </div>
+      </Link>
+
       <div className="ml-auto flex items-center">
         <FollowButton
           isFollowing={userInfo.status === 'FOLLOWING'}
