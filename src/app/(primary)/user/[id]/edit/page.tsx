c3400d8a60dfa1df4b39876207ecaf777b80bcbb
hyejj19@naver.com <hyejj19@naver.com>
2025-01-30 23:52:33 +0900

    debug

--- src/app/(primary)/user/[id]/edit/page.tsx
+++ src/app/(primary)/user/[id]/edit/page.tsx
@@ -97,7 +97,8 @@ export default function UserEditPage() {
     }
 
     window.openAlbum = (imgDataBase64) => {
-      setImageData(imgDataBase64);
+      console.log('데이터~', imgDataBase64);
+      // setImageData(imgDataBase64);
     };
 
     window.checkIsInApp = checkIsInApp;
