5e51f136306c652bcb3327e6a633a22015a97a2c
sareum <rkdtkfma@gmail.com>
2025-02-15 12:26:51 +0900

    [Style]modify layout when show HistoryEmptyState component.

--- src/app/(primary)/history/page.tsx
+++ src/app/(primary)/history/page.tsx
@@ -62,7 +62,9 @@ export default function History() {
     return { year, month };
   }
 
-  // !! 회색 라인 오류 수정하기
+  const dataChecking =
+    historyData && historyData[0].data.totalCount !== 0 && !error && !isLoading;
+
   // !! 보틀노트 시작한 날짜 뽑기
   // !! 무한 스크롤 구현
   // !! 필터 구현
@@ -94,9 +96,11 @@ export default function History() {
           showRecentSearch={false}
         />
         <section className="p-5 mb-10">
-          <div className="flex items-center justify-between">
-            {historyData && (
+          <div className="flex items-center justify-between mb-[0.65rem]">
+            {historyData ? (
               <span className="text-xs text-mainGray shrink-0">{`총 ${historyData[0].data.totalCount}개`}</span>
+            ) : (
+              <span />
             )}
             <div className="flex items-center">
               <Image src={DescendingIcon} alt="내림차순" />
@@ -107,71 +111,82 @@ export default function History() {
               />
             </div>
           </div>
-          <div className="border-t border-mainGray/30 my-[0.65rem]" />
+          {dataChecking && (
+            <>
+              <div className="border-t border-mainGray/30 mb-[0.65rem]" />
 
-          <article className="relative w-[339px]">
-            <div className="absolute left-[2.7rem] top-6 bottom-0 w-px border-l border-dashed border-subCoral z-0" />
-            <div className="text-10 text-mainGray bg-bgGray rounded-md p-2 mb-5 ml-3 relative z-10">
-              {getLatestYearMonth()?.year}년 {getLatestYearMonth()?.month}월까지
-              기록된 회원닉네임님의 활동여정이에요!
-            </div>
-            <div className="relative z-10 pb-3">
-              {yearMonths.map((yearMonth, index) => {
-                const items = groupedHistory[yearMonth];
-                return (
-                  <div key={yearMonth} className="relative">
-                    {yearMonth !== yearMonths[0] && (
-                      <div className="pl-4 mb-5">
-                        <Label
-                          name={yearMonth}
-                          styleClass="border-white px-2.5 py-1 rounded-md text-11 bg-bgGray text-subCoral"
-                        />
-                      </div>
-                    )}
-                    <div className="z-10 space-y-5">
-                      {items.map((item: HistoryType, itemIndex) => {
-                        const prevItem =
-                          itemIndex > 0 ? items[itemIndex - 1] : null;
-                        const showDivider = shouldShowDivider(item, prevItem);
-                        return (
-                          <React.Fragment key={item.historyId}>
-                            {showDivider && (
-                              <div className="relative py-1">
-                                <div className="absolute left-0 right-0 h-px bg-bgGray" />
-                              </div>
-                            )}
-                            <TimeLineItem
-                              date={item.createdAt}
-                              alcoholName={item.alcoholName}
-                              imageSrc={item.imageUrl}
-                              type={item.eventType}
-                              rate={item.dynamicMessage}
-                              text={item.message}
-                              alcoholId={item.alcoholId}
+              <article className="relative w-[339px]">
+                <div className="absolute left-[2.7rem] top-6 bottom-0 w-px border-l border-dashed border-subCoral z-0" />
+                <div className="text-10 text-mainGray bg-bgGray rounded-md p-2 mb-5 ml-3 relative z-10">
+                  {getLatestYearMonth()?.year}년 {getLatestYearMonth()?.month}
+                  월까지 기록된 회원닉네임님의 활동여정이에요!
+                </div>
+                <div className="relative z-10 pb-3">
+                  {yearMonths.map((yearMonth, index) => {
+                    const items = groupedHistory[yearMonth];
+                    return (
+                      <div key={yearMonth} className="relative">
+                        {yearMonth !== yearMonths[0] && (
+                          <div className="pl-4 mb-5">
+                            <Label
+                              name={yearMonth}
+                              styleClass="border-white px-2.5 py-1 rounded-md text-11 bg-bgGray text-subCoral"
                             />
-                          </React.Fragment>
-                        );
-                      })}
-                    </div>
-                    {index !== yearMonths.length - 1 && (
-                      <div className="my-5" />
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-            <div className="relative z-10 pb-3 mt-5">
-              <TimeLineItem isStart date="2024-01-19T14:35:12" type="BOTTLE" />
-            </div>
-          </article>
+                          </div>
+                        )}
+                        <div className="z-10 space-y-5">
+                          {items.map((item: HistoryType, itemIndex) => {
+                            const prevItem =
+                              itemIndex > 0 ? items[itemIndex - 1] : null;
+                            const showDivider = shouldShowDivider(
+                              item,
+                              prevItem,
+                            );
+                            return (
+                              <React.Fragment key={item.historyId}>
+                                {showDivider && (
+                                  <div className="relative py-1">
+                                    <div className="absolute left-0 right-0 h-px bg-bgGray" />
+                                  </div>
+                                )}
+                                <TimeLineItem
+                                  date={item.createdAt}
+                                  alcoholName={item.alcoholName}
+                                  imageSrc={item.imageUrl}
+                                  type={item.eventType}
+                                  rate={item.dynamicMessage}
+                                  text={item.message}
+                                  alcoholId={item.alcoholId}
+                                />
+                              </React.Fragment>
+                            );
+                          })}
+                        </div>
+                        {index !== yearMonths.length - 1 && (
+                          <div className="my-5" />
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="relative z-10 pb-3 mt-5">
+                  <TimeLineItem
+                    isStart
+                    date="2024-01-19T14:35:12"
+                    type="BOTTLE"
+                  />
+                </div>
+              </article>
+            </>
+          )}
+          <HistoryEmptyState
+            isLoading={isLoading}
+            error={error}
+            totalCount={historyData?.[0]?.data.totalCount}
+          />
         </section>
       </main>
       <FilterSideModal isOpen={isOpen} onClose={handleClose} />
-      <HistoryEmptyState
-        isLoading={isLoading}
-        error={error}
-        totalCount={historyData?.[0]?.data.totalCount}
-      />
     </NavLayout>
   );
 }
