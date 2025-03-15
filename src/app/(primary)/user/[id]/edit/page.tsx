b7c440f9a8e42fa57ebe380a83cd1c96534dc2b0
hyejj19@naver.com <hyejj19@naver.com>
2025-03-16 08:22:17 +0900

    chore: delete unused useEffect

--- src/app/(primary)/user/[id]/edit/page.tsx
+++ src/app/(primary)/user/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { SubHeader } from '@/app/(primary)/_components/SubHeader';
