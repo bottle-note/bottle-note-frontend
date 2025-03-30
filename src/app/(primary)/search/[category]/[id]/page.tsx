d4c340845ddfc88dc0fdbb1c675ac98d0cf9ec93
sareum <rkdtkfma@gmail.com>
2025-03-30 14:17:10 +0900

    [Style] change cask location.

--- src/app/(primary)/search/[category]/[id]/page.tsx
+++ src/app/(primary)/search/[category]/[id]/page.tsx
@@ -51,11 +51,12 @@ function SearchAlcohol() {
         setData(result);
         setIsPicked(alcohols.isPicked);
         setAlcoholDetails([
-          { title: '카테고리', content: alcohols.engCategory || '-' },
-          { title: '국가/지역', content: alcohols.engRegion || '-' },
-          { title: '캐스크', content: alcohols.cask || '-' },
-          { title: '도수(%)', content: alcohols.abv || '-' },
-          { title: '증류소', content: alcohols.engDistillery || '-' },
+          { title: '증류소', content: alcohols.engDistillery },
+          {
+            title: '국가/지역',
+            content: alcohols.engRegion?.replace('/', '/\n') || '-',
+          },
+          { title: '도수', content: `${alcohols.abv}%` },
         ]);
       }
     } catch (error) {
@@ -195,7 +196,7 @@ function SearchAlcohol() {
               />
             </div>
             <div className="mb-5">
-              <article className="grid place-items-center space-y-2 py-5">
+              <article className="grid place-items-center space-y-2 pt-[25px] pb-[21px]">
                 {getRatingMessage(
                   data?.alcohols?.myAvgRating,
                   data?.alcohols?.myRating,
@@ -204,16 +205,30 @@ function SearchAlcohol() {
                   <StarRating rate={rate} size={42} handleRate={handleRate} />
                 </div>
               </article>
-              <section className="mx-5 py-5 border-y border-mainGray/30 grid grid-cols-2 gap-2">
-                {alcoholDetails.map((item: DetailItem) => (
-                  <div
-                    key={item.content}
-                    className="flex text-12 text-mainDarkGray items-start gap-2"
-                  >
-                    <div className="min-w-14 font-semibold">{item.title}</div>
-                    <div className="flex-1 font-normal">{item.content}</div>
+              <section className="mx-5 py-[21px] border-y border-mainGray/30">
+                <div className="grid gap-2">
+                  <div className="grid grid-cols-2 gap-2">
+                    {alcoholDetails.map((item: DetailItem) => (
+                      <div
+                        key={item.content}
+                        className="flex text-12 text-mainDarkGray items-start gap-2"
+                      >
+                        <div className="min-w-14 font-semibold">
+                          {item.title}
+                        </div>
+                        <div className="flex-1 font-normal whitespace-pre-line">
+                          {item.content}
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                ))}
+                  <div className="flex text-12 text-mainDarkGray items-start gap-2">
+                    <div className="min-w-14 font-semibold">캐스크</div>
+                    <div className="flex-1 font-normal break-words">
+                      {data?.alcohols?.cask || '-'}
+                    </div>
+                  </div>
+                </div>
               </section>
               {data?.alcohols?.alcoholsTastingTags && (
                 <FlavorTag tagList={data.alcohols.alcoholsTastingTags} />
