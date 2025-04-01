935fab2fa7a798bb7ef7e797c9e43937924fc98d
SAREUM-K <sarumi@ntelslabs.com>
2025-04-02 08:55:33 +0900

    [Modify] Add Fallback image of alcohol.

--- src/app/(primary)/_components/AlcoholImage.tsx
+++ src/app/(primary)/_components/AlcoholImage.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
+import Fallback from 'public/bottle.svg';
 
 interface Props {
   imageUrl: string;
@@ -14,23 +18,28 @@ const AlcoholImage = ({
   outerWidthClass = 'w-[100px]',
   innerHeightClass = 'w-[80px] ',
   innerWidthClass = 'h-[140px]',
-}: Props) => (
-  <div className="rounded-lg bg-white flex items-center justify-center">
-    <article
-      className={`${outerHeightClass} ${outerWidthClass} shrink-0 relative flex items-center justify-center`}
-    >
-      <div className={`${innerHeightClass} ${innerWidthClass} relative`}>
-        <Image
-          priority
-          src={imageUrl}
-          alt="alcohol image"
-          fill
-          className="object-contain"
-          sizes="100px"
-        />
-      </div>
-    </article>
-  </div>
-);
+}: Props) => {
+  const [imgSrc, setImgSrc] = useState(imageUrl);
+
+  return (
+    <div className="rounded-lg bg-white flex items-center justify-center">
+      <article
+        className={`${outerHeightClass} ${outerWidthClass} shrink-0 relative flex items-center justify-center`}
+      >
+        <div className={`${innerHeightClass} ${innerWidthClass} relative`}>
+          <Image
+            priority
+            src={imgSrc}
+            alt="alcohol image"
+            fill
+            className="object-contain"
+            sizes="100px"
+            onError={() => setImgSrc(Fallback)}
+          />
+        </div>
+      </article>
+    </div>
+  );
+};
 
 export default AlcoholImage;
