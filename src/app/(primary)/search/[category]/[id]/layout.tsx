2333e9c4e72821917e6d448c8f9a3009546ddd72
SAREUM-K <sarumi@ntelslabs.com>
2025-04-02 10:31:52 +0900

    [Style] modify to ensure the header area and layout remain intact.

--- src/app/(primary)/search/[category]/[id]/layout.tsx
+++ src/app/(primary)/search/[category]/[id]/layout.tsx
@@ -5,5 +5,5 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return <div className="min-h-screen relative">{children}</div>;
 }
