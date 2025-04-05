902e7e23cacf753e01917e37fcb50524928666e0
hyejj19@naver.com <hyejj19@naver.com>
2025-04-05 18:24:20 +0900

    fix: minor changes

--- src/hooks/useAppSocialLogin.ts
+++ src/hooks/useAppSocialLogin.ts
@@ -26,10 +26,15 @@ export const useAppSocialLogin = () => {
   const onAppleLoginError = (error: string) => {
     console.error(`❌ 애플 로그인 실패: ${error}`);
     sendLogToFlutter(error);
+    // TODO: 에러 메시지를 받아와서 모달을 띄워주는 액션 필요
+    // 로그인 설계에서 에러 바운더리를 지역적으로 시범적용, 글로벌로 점차 퍼져나가도록 설계 변경 필요해보임.
+    // fsd 형식으로 전체 설계를 바꿔버릴까?
   };
 
   const onAppleLoginSuccess = async (data: string) => {
     try {
+      sendLogToFlutter(`data : ${data}, ${typeof data}`);
+
       // 여기서 정보가 제대로 오지 않았을 경우 실패 처리 필요
       const { email, id } = JSON.parse(data) as {
         email: string | null;
