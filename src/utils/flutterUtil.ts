8a534b37695ae7f3de4299116ef50d8e21b9320d
hyejj19@naver.com <hyejj19@naver.com>
2025-04-22 20:32:26 +0900

    modify: flutterUtil to update DeviceService state

--- src/utils/flutterUtil.ts
+++ src/utils/flutterUtil.ts
@@ -3,19 +3,20 @@ import { DeviceService } from '@/lib/DeviceService';
 export function checkIsInApp(status: string) {
   const result = status === 'false' ? false : Boolean(status);
   window.isInApp = result;
+  DeviceService.setIsInApp(result);
 
   return result;
 }
 
 export function checkPlatform(platform: string) {
   window.platform = platform;
+  DeviceService.setPlatform(platform);
 
   return platform;
 }
 
 export function getDeviceToken(token: string, platform: string) {
   DeviceService.setDeviceToken(token);
-  DeviceService.setPlatform(platform);
 
   return { deviceToken: token, platform };
 }
