3270b3c33995f6d8f9009ea8cab213933a32e7df
sareum-k <rkdtkfma@gmail.com>
2025-06-17 10:08:28 +0900

    [Style] modify layout of mypage

--- src/app/(primary)/user/[id]/page.tsx
+++ src/app/(primary)/user/[id]/page.tsx
@@ -22,24 +22,26 @@ export default function User({ params: { id } }: { params: { id: string } }) {
   return (
     <NavLayout>
       <main className="text-mainBlack mb-24">
-        <section className="bg-bgGray px-7.5 pb-[35px]">
+        <section className="bg-white px-7.5 pb-[35px]">
           <SidebarHeader />
-          <UserInfo
-            profileImgSrc={userData?.imageUrl ?? null}
-            follower={userData?.followerCount ?? 0}
-            following={userData?.followingCount ?? 0}
-            isFollowing={userData?.isFollow}
-            currentId={id}
-            nickName={userData?.nickName ?? ''}
-          />
-          <HistoryOverview
-            rates={userData?.ratingCount ?? 0}
-            reviews={userData?.reviewCount ?? 0}
-            likes={userData?.pickCount ?? 0}
-            id={Number(id)}
-          />
+          <section className="border-y border-subCoral pb-[22px]">
+            <UserInfo
+              profileImgSrc={userData?.imageUrl ?? null}
+              follower={userData?.followerCount ?? 0}
+              following={userData?.followingCount ?? 0}
+              isFollowing={userData?.isFollow}
+              currentId={id}
+              nickName={userData?.nickName ?? ''}
+            />
+            <HistoryOverview
+              rates={userData?.ratingCount ?? 0}
+              reviews={userData?.reviewCount ?? 0}
+              likes={userData?.pickCount ?? 0}
+              id={Number(id)}
+            />
+          </section>
         </section>
-        <section className="px-5 pt-9">
+        <section className="px-5 pt-8">
           <Timeline />
         </section>
       </main>
