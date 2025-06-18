17d22127b177f721e30a66c201b31f9b8c11d21d
Hyejung Park <hyejj19@naver.com>
2025-06-19 08:38:46 +0900

    [#158] 키보드 입력시 웹뷰 화면 이동 버그 개선 외. (#488)
    * feat: scroll into input when focus input or textarea
    
    * chore: add switchEnv handler

--- src/app/(primary)/_components/SidebarHeader.tsx
+++ src/app/(primary)/_components/SidebarHeader.tsx
@@ -11,6 +11,7 @@ import { AuthService } from '@/lib/AuthService';
 import useModalStore from '@/store/modalStore';
 import Modal from '@/components/Modal';
 import { UserApi } from '@/app/api/UserApi';
+import { handleWebViewMessage } from '@/utils/flutterUtil';
 import Logo from 'public/bottle_note_Icon_logo.svg';
 import LogoWhite from 'public/bottle_note_Icon_logo_white.svg';
 import Menu from 'public/icon/menu-subcoral.svg';
@@ -28,10 +29,42 @@ const Header = ({
   const menuSrc = isOpen ? MenuWhite : Menu;
   const menuAlt = isOpen ? '메뉴 닫기' : '메뉴 열기';
 
+  /*
+  - 개발 <-> 상용 변경 핸들러
+  - 사이드바에서 특정 유저의 경우 버튼 노출 및 핸들러 적용 (기획 반영 예정)
+  - 우선적으로 로고에 적용합니다.
+  - 기능 동작을 확인하는 임시 로직이라서 추후 삭제 또는 수정 예정이니 참고부탁드립니다.
+  */
+
+  const [count, setCount] = useState(0);
+  const { handleModalState, handleCloseModal } = useModalStore();
+  const handleSwitchEnv = (env: 'dev' | 'prod') => {
+    handleWebViewMessage('switchEnv', env);
+    handleCloseModal();
+  };
+
+  useEffect(() => {
+    if (count === 5) {
+      handleModalState({
+        isShowModal: true,
+        type: 'CONFIRM',
+        mainText: '개발 환경으로 전환하시겠습니까?',
+        confirmBtnName: '개발',
+        cancelBtnName: '상용',
+        handleConfirm: () => handleSwitchEnv('dev'),
+        handleCancel: () => handleSwitchEnv('prod'),
+      });
+      setCount(0);
+    }
+  }, [count]);
+
   if (isOpen) {
     return (
       <article className="flex justify-between bg-subCoral px-[17px] pb-[15px] pt-[74px]">
-        <button>
+        <button onClick={() => setCount(count + 1)} className="relative">
+          <span className="absolute top-2 left-1 text-xs text-textGray">
+            {count ? count : ''}
+          </span>
           <Image src={logoSrc} alt="보틀노트" />
         </button>
         <button onClick={handleOpen}>
