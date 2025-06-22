39354b8fd517c791073508888c482402b4e2e43d
sareum <rkdtkfma@gmail.com>
2025-06-22 09:16:16 +0900

    [Modify] change font-size of textarea and delete private logic.

--- src/app/(primary)/review/_components/form/ContentForm.tsx
+++ src/app/(primary)/review/_components/form/ContentForm.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
-import Toggle from '@/app/(primary)/_components/Toggle';
 
 export default function ContentForm() {
-  const { register, setValue, watch } = useFormContext();
-
-  const handleToggle = () => {
-    setValue('status', watch('status') === 'PUBLIC' ? 'PRIVATE' : 'PUBLIC');
-  };
+  const { register } = useFormContext();
 
   return (
     <article>
       <textarea
         {...register('review')}
         placeholder="이 위스키에 대한 리뷰를 작성해보세요. (최대 1,000자)"
-        className="text-12 w-full h-48"
+        className="text-16 w-full h-48"
         maxLength={1000}
       />
-      <div className="text-12 text-mainGray flex justify-between items-center">
-        <Toggle
-          isActive={watch('status') === 'PUBLIC'}
-          onToggle={handleToggle}
-        />
-        <div>({watch('review')?.length}/1000)</div>
-      </div>
     </article>
   );
 }
