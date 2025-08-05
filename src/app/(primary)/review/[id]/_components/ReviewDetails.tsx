d92b3088f06104a0112fbacfdbc10d09beff7154
sareum <rkdtkfma@gmail.com>
2025-08-05 21:28:58 +0900

    [Feat] implement ReviewActions, ReviewPriceLocation, and ReviewUserHeader components; refactor ReviewDetails to integrate new components

--- src/app/(primary)/review/[id]/_components/ReviewDetails.tsx
+++ src/app/(primary)/review/[id]/_components/ReviewDetails.tsx
@@ -1,16 +1,6 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import Label from '@/app/(primary)/_components/Label';
-import { truncStr } from '@/utils/truncStr';
-import Star from '@/components/Star';
-import VisibilityToggle from '@/app/(primary)/_components/VisibilityToggle';
 import FlavorTag from '@/app/(primary)/_components/FlavorTag';
-import { numberWithCommas } from '@/utils/formatNum';
-import { formatDate } from '@/utils/formatDate';
-// import { shareOrCopy } from '@/utils/shareOrCopy';
-import LikeBtn from '@/app/(primary)/_components/LikeBtn';
 import OptionDropdown from '@/components/OptionDropdown';
 import useModalStore from '@/store/modalStore';
 import { ReviewDetailsWithoutAlcoholInfo } from '@/types/Review';
@@ -21,7 +11,9 @@ import {
   ReviewImageCarousel,
   convertImageUrlsToProductImageArray,
 } from '@/app/(primary)/_components/ReviewImageCarousel';
-import ProfileDefaultImg from 'public/profile-default.svg';
+import ReviewUserHeader from './ReviewUserHeader';
+import ReviewPriceLocation from './ReviewPriceLocation';
+import ReviewActions from './ReviewActions';
 
 interface Props {
   data: ReviewDetailsWithoutAlcoholInfo;
@@ -31,8 +23,8 @@ interface Props {
 
 function ReviewDetails({ data, handleLogin, onRefresh }: Props) {
   const router = useRouter();
-  const { userData, isLogin } = AuthService;
-  const { handleModalState, handleLoginModal } = useModalStore();
+  const { userData } = AuthService;
+  const { handleModalState } = useModalStore();
   const [isOptionShow, setIsOptionShow] = useState(false);
   const [isLiked, setIsLiked] = useState(data?.reviewInfo?.isLikedByMe);
   const [likeCount, setLikeCount] = useState(data?.reviewInfo?.likeCount);
@@ -77,67 +69,15 @@ function ReviewDetails({ data, handleLogin, onRefresh }: Props) {
     }
   };
 
-  const hasValidPrice = data.reviewInfo?.price || data.reviewInfo?.price === 0;
-  const hasValidSizeType = data.reviewInfo?.sizeType;
-  const hasValidLocation = data.reviewInfo?.locationInfo?.address;
-  const shouldShowPriceOrLocation =
-    hasValidLocation || (hasValidPrice && hasValidSizeType);
-
   return (
     <>
       <section className="pt-[38px]">
-        <section className="mx-5 pb-5 border-b border-mainGray/30 ">
-          <article className="flex items-center justify-between">
-            <Link href={ROUTES.USER.BASE(userData?.userId!)}>
-              <div className="flex items-center space-x-[7px] ">
-                <div className="w-[1.9rem] h-[1.9rem] rounded-full overflow-hidden">
-                  <Image
-                    className="object-cover"
-                    src={
-                      data.reviewInfo?.userInfo?.userProfileImage ??
-                      ProfileDefaultImg
-                    }
-                    alt="user_img"
-                    width={30}
-                    height={30}
-                  />
-                </div>
-                <p className="text-mainGray text-13">
-                  {data.reviewInfo?.userInfo?.nickName &&
-                    truncStr(data.reviewInfo.userInfo.nickName, 12)}
-                </p>
-              </div>
-            </Link>
-            <Star
-              rating={data.reviewInfo?.rating ?? 0}
-              size={21}
-              textStyle="text-20 text-subCoral font-semibold w-7"
-            />
-          </article>
-          <article className="flex items-center mt-[10px] space-x-2">
-            {data.reviewInfo?.isBestReview && (
-              <Label
-                name="베스트"
-                icon="/icon/thumbup-filled-white.svg"
-                styleClass="bg-mainCoral text-white px-2 py-[0.1rem] border-mainCoral text-10 rounded"
-              />
-            )}
-            {data.reviewInfo?.isMyReview && (
-              <Label
-                name="나의 코멘트"
-                icon="/icon/user-outlined-subcoral.svg"
-                styleClass="border-mainCoral text-mainCoral px-2 py-[0.1rem] text-10 rounded"
-              />
-            )}
-            {data.reviewInfo?.userInfo?.userId === userData?.userId && (
-              <VisibilityToggle
-                initialStatus={data.reviewInfo.status === 'PUBLIC'}
-                reviewId={data?.reviewInfo?.reviewId}
-                handleNotLogin={handleLoginModal}
-                onSuccess={onRefresh}
-              />
-            )}
-          </article>
+        <ReviewUserHeader
+          data={data}
+          onRefresh={onRefresh}
+          onOptionClick={() => setIsOptionShow(true)}
+        />
+        <section className="mx-5 pb-5 border-b border-mainGray/30">
           {productImages && (
             <div className="my-[22px]">
               <ReviewImageCarousel images={productImages} />
@@ -149,26 +89,6 @@ function ReviewDetails({ data, handleLogin, onRefresh }: Props) {
               __html: data.reviewInfo?.reviewContent?.replace(/\n/g, '<br />'),
             }}
           />
-          <article className="flex justify-between mt-[10px]">
-            {data.reviewInfo?.createAt && (
-              <p className="text-mainGray text-10">
-                {formatDate(data.reviewInfo.createAt) as string}
-              </p>
-            )}
-            <button
-              className="cursor-pointer"
-              onClick={() => {
-                setIsOptionShow(true);
-              }}
-            >
-              <Image
-                src="/icon/ellipsis-darkgray.svg"
-                width={14}
-                height={14}
-                alt="report"
-              />
-            </button>
-          </article>
         </section>
         {data.reviewInfo?.tastingTagList?.length &&
           data.reviewInfo.tastingTagList.length !== 0 && (
@@ -177,135 +97,25 @@ function ReviewDetails({ data, handleLogin, onRefresh }: Props) {
               styleClass="border-subCoral text-subCoral py-[5px] px-[10px] rounded-md text-12"
             />
           )}
-        {shouldShowPriceOrLocation && (
-          <section className="mx-5 py-5 space-y-2 border-b border-mainGray/30 text-12">
-            {hasValidPrice && hasValidSizeType && (
-              <div className="flex items-center space-x-[6px]">
-                <Image
-                  src={
-                    data.reviewInfo.sizeType === 'BOTTLE'
-                      ? '/bottle.svg'
-                      : '/icon/glass-filled-subcoral.svg'
-                  }
-                  width={17}
-                  height={17}
-                  alt={
-                    data.reviewInfo.sizeType === 'BOTTLE'
-                      ? 'Bottle Price'
-                      : 'Glass Price'
-                  }
-                />
-                <p className="text-mainDarkGray font-bold">
-                  {data.reviewInfo.sizeType === 'BOTTLE'
-                    ? '병 가격 '
-                    : '잔 가격'}
-                </p>
-                <p className="text-mainDarkGray font-normal">
-                  {numberWithCommas(data.reviewInfo.price)}₩
-                </p>
-              </div>
-            )}
-            {hasValidLocation && (
-              <div className="flex items-start space-x-[6px]">
-                <Image
-                  src="/icon/placepoint-subcoral.svg"
-                  width={17}
-                  height={17}
-                  alt="address"
-                />
-                <p className="text-mainDarkGray font-bold">장소</p>
-                <p className="text-mainDarkGray">
-                  <>
-                    <p>{data.reviewInfo?.locationInfo?.name}</p>
-                    {data.reviewInfo?.locationInfo?.address}{' '}
-                    {data.reviewInfo?.locationInfo?.detailAddress}{' '}
-                    <a
-                      href={data.reviewInfo?.locationInfo?.mapUrl || '#'}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        const mapUrl = data.reviewInfo?.locationInfo?.mapUrl;
-                        if (mapUrl) {
-                          window.open(mapUrl, '_blank', 'noopener,noreferrer');
-                        }
-                      }}
-                      className="text-subCoral cursor-pointer"
-                    >
-                      지도보기
-                    </a>
-                  </>
-                </p>
-              </div>
-            )}
-          </section>
-        )}
-        <section className="mx-5 py-5 flex items-center space-x-4">
-          <div className="flex-1 flex text-center justify-center items-center space-x-1">
-            <LikeBtn
-              size={16}
-              reviewId={data?.reviewInfo?.reviewId}
-              isLiked={isLiked}
-              handleUpdateLiked={() => {
-                setIsLiked((prev) => !prev);
-                setLikeCount((prev) => (isLiked ? prev - 1 : prev + 1));
-              }}
-              onApiError={() => {
-                setLikeCount(data?.reviewInfo?.likeCount);
-                setIsLiked(data?.reviewInfo?.isLikedByMe);
-              }}
-              handleNotLogin={handleLogin}
-              likeBtnName="좋아요"
-            />
-            <div className="text-mainGray text-10">{likeCount}개</div>
-          </div>
 
-          <span className="border-[0.01rem] w-px border-mainGray opacity-40 h-4" />
+        {/* 가격 및 위치 정보 */}
+        <ReviewPriceLocation data={data} />
 
-          <button
-            className="flex-1 flex text-center justify-center items-center space-x-1"
-            onClick={() => {
-              if (!isLogin) handleLogin();
-            }}
-          >
-            <Image
-              src={
-                data.reviewInfo?.hasReplyByMe
-                  ? 'icon/comment-filled-subcoral.svg'
-                  : '/icon/comment-outlined-gray.svg'
-              }
-              width={16}
-              height={16}
-              alt="comment"
-            />
-            <p className="relative w-fit text-mainGray font-bold text-13">
-              댓글 작성
-            </p>
-          </button>
-
-          <span className="border-[0.01rem] w-px border-mainGray opacity-40 h-4" />
-
-          <button
-            className="flex-1 flex text-center justify-center items-center space-x-1"
-            onClick={() => {
-              // shareOrCopy(
-              //   `${process.env.NEXT_PUBLIC_BOTTLE_NOTE_URL}/review/${data.reviewInfo?.reviewId}`,
-              // handleModalState,
-              // );
-              handleModalState({
-                isShowModal: true,
-                type: 'ALERT',
-                mainText: '아직 준비 중인 기능입니다. 조금만 기다려주세요!',
-              });
-            }}
-          >
-            <Image
-              src="/icon/externallink-outlined-gray.svg"
-              alt="linkIcon"
-              width={16}
-              height={16}
-            />
-            <p className="text-mainGray font-bold text-13">공유</p>
-          </button>
-        </section>
+        {/* 좋아요, 댓글, 공유 버튼 */}
+        <ReviewActions
+          data={data}
+          isLiked={isLiked}
+          likeCount={likeCount}
+          onLikeUpdate={() => {
+            setIsLiked((prev) => !prev);
+            setLikeCount((prev) => (isLiked ? prev - 1 : prev + 1));
+          }}
+          onLikeError={() => {
+            setLikeCount(data?.reviewInfo?.likeCount);
+            setIsLiked(data?.reviewInfo?.isLikedByMe);
+          }}
+          handleLogin={handleLogin}
+        />
       </section>
       {isOptionShow && (
         <OptionDropdown
