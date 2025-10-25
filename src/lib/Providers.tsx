23d109b5df527b766395aad651043c43888366d6
sareum <rkdtkfma@gmail.com>
2025-10-25 17:32:53 +0900

    [Refactor] move AuthInitializer logic into Providers component

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -4,12 +4,17 @@ import React, { ReactNode, useState } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthInitializer } from '@/components/AuthInitializer';
+import { useAuthInitializer } from '@/hooks/useAuthInitializer';
 import Modal from '@/components/ui/Modal/Modal';
 import { FocusScrollProvider } from './FocusScrollProvider';
 
 interface Props {
   children: ReactNode;
+}
+
+function AuthInitializer() {
+  useAuthInitializer();
+  return null;
 }
 
 export const Providers = ({ children }: Props) => {
@@ -18,8 +23,6 @@ export const Providers = ({ children }: Props) => {
       new QueryClient({
         defaultOptions: {
           queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
             staleTime: 60 * 1000,
             retry: 1,
           },
