8f7da3cd880a79b9d21e532e17529a3ba46b3aa6
sareum <rkdtkfma@gmail.com>
2025-10-25 19:16:13 +0900

    [Fix] adjust margin in ReviewDetails and refactor conditional rendering in ReviewUserHeader

--- src/app/(primary)/review/[id]/_components/ReviewDetails.tsx
+++ src/app/(primary)/review/[id]/_components/ReviewDetails.tsx
@@ -34,7 +34,7 @@ function ReviewDetails({ data, handleLogin, onRefresh, textareaRef }: Props) {
   return (
     <>
       <section className="pt-[38px]">
-        <div className="mb-[22px]">
+        <div className="mb-[10px]">
           <ReviewUserHeader data={data} onRefresh={onRefresh} />
         </div>
         <section className="mx-5 pb-5 border-b border-mainGray/30">
